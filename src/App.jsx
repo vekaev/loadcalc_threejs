db6a6b27884a3e5c520faeaf3e3f6fda3c6262ce
Vekaev <vekaev4@gmail.com>
2020-09-04 18:14:52 +0300

    complete final render

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,8 @@ import React, { useEffect, useRef, useState } from 'react';
 import './main.scss';
 import * as THREE from 'three/build/three.module.js';
 import { createBox, createCylinder } from './utils/index';
-
+import { responceData } from './responceData';
+import { showResult } from './utils';
 const OrbitControls = require('three-orbit-controls')(THREE);
 
 const defaultParam = {
@@ -17,7 +18,7 @@ const boxData = {
     y: 990,
     z: 1200,
   },
-  position: {
+  location: {
     x: 200,
     y: 0,
     z: 880,
@@ -27,20 +28,23 @@ const boxData = {
     y: 3,
     z: 5,
   },
+  info:
+    '20x20 container info 123456789 hello worldgffretrfgdfgdfg dfsdsdfsdfsdfs',
 };
 
 const App = () => {
-  const el = useRef(null);
+  const [palletView, setPalletView] = useState(false);
+  const canvas = useRef(null);
   const scene = new THREE.Scene();
 
   useEffect(() => {
     const renderer = new THREE.WebGLRenderer();
-    renderer.setClearColor(0x000000);
+    renderer.setClearColor(0xffffff);
     // f8fafd
     renderer.setSize(WIDTH, HEIGHT);
 
     const camera = new THREE.PerspectiveCamera(75, WIDTH / HEIGHT, 0.5, 1000);
-    camera.position.set(15, 15, 30);
+    camera.position.set(20, 15, 25);
 
     //SET USER CONTROL AND ZOOM LIMIT
     const controls = new OrbitControls(camera);
@@ -49,12 +53,13 @@ const App = () => {
 
     const light = new THREE.AmbientLight(0xffffff, 5.0);
 
-    const gridHelper = new THREE.GridHelper(100, 20, 0xb6bdc4, 0xb6bdc4);
+    const gridHelper = new THREE.GridHelper(400, 20, 0xb6bdc4, 0xb6bdc4);
 
-    createCylinder(scene);
-    createBox(scene, boxData);
+    responceData.forEach((item) => {
+      showResult(scene, item);
+    });
 
-    el.current.appendChild(renderer.domElement);
+    canvas.current.appendChild(renderer.domElement);
 
     scene.add(light, gridHelper);
 
@@ -72,7 +77,19 @@ const App = () => {
     });
   });
 
-  return <div ref={el}></div>;
+  return (
+    <>
+      {/*<button*/}
+      {/*  style={{ position: 'fixed' }}*/}
+      {/*  onClick={() => {*/}
+      {/*    setPalletView(!palletView);*/}
+      {/*  }}*/}
+      {/*>*/}
+      {/*  ShowPallet*/}
+      {/*</button>*/}
+      <div ref={canvas}></div>
+    </>
+  );
 };
 
 export default App;
--- src/utils/index.js
+++ src/utils/index.js
@@ -1,11 +1,40 @@
 import * as THREE from 'three/build/three.module.js';
 
-const convertCoordinatesCube = (position, size) => {
-  let calc = position + size / 2;
+const sizeCoefficient = 50;
+
+export const getRandomColor = () => {
+  let letters = '0123456789ABCDEF';
+  let color = '#';
+  for (let i = 0; i < 6; i++) color += letters[Math.floor(Math.random() * 16)];
+  return color;
+};
+
+const convertCoordinatesCube = (location, size) => {
+  let calc = location + size / 2;
   return calc;
 };
 
-export const createBox = (scene, { size, complex, position }) => {
+function wrapText(context, text, x, y, maxWidth, lineHeight) {
+  let words = text.split(' ');
+  let line = '';
+
+  for (let n = 0; n < words.length; n++) {
+    let testLine = line + words[n] + ' ';
+    let metrics = context.measureText(testLine);
+    let testWidth = metrics.width;
+    if (testWidth > maxWidth && n > 0) {
+      context.fillText(line, x, y);
+      line = words[n] + ' ';
+      y += lineHeight;
+    } else {
+      line = testLine;
+    }
+  }
+  context.fillText(line, x, y);
+}
+
+export const createBox = (scene, { size, complex, location, uid }) => {
+  const color = getRandomColor();
   const createCanvasBcg = (
     canvas,
     ctx,
@@ -16,7 +45,7 @@ export const createBox = (scene, { size, complex, position }) => {
     for (let i = 0; i <= secondAxis; i++) {
       for (let j = 0; j <= firstAxis; j++) {
         ctx.fillStyle = color;
-        ctx.lineWidth = 2;
+        ctx.lineWidth = 5;
         ctx.strokeStyle = '#fff';
         ctx.fillRect(
           j * (canvas.width / firstAxis),
@@ -36,18 +65,19 @@ export const createBox = (scene, { size, complex, position }) => {
   };
 
   const convertData = {
-    xw: size.x / 100,
-    yw: size.y / 100,
-    zw: size.z / 100,
+    xw: size.x / sizeCoefficient,
+    yw: size.y / sizeCoefficient,
+    zw: size.z / sizeCoefficient,
     xq: complex.x,
     yq: complex.y,
     zq: complex.z,
-    posX: position.x / 100,
-    posY: position.y / 100,
-    posZ: position.z / 100,
+    posX: location.x / sizeCoefficient,
+    posY: location.y / sizeCoefficient,
+    posZ: location.z / sizeCoefficient,
+    text: uid,
   };
 
-  const { xw, xq, yw, yq, zw, zq, posX, posY, posZ } = convertData;
+  const { xw, xq, yw, yq, zw, zq, posX, posY, posZ, text } = convertData;
 
   let cubeGeometry = new THREE.BoxGeometry(xw, zw, yw);
 
@@ -56,24 +86,37 @@ export const createBox = (scene, { size, complex, position }) => {
     [xq, zq],
     [yq, zq],
   ];
+  const axisWidth = [
+    [xw, yw],
+    [xw, zw],
+    [yw, zw],
+  ];
   let texture = [];
   let canvas = [];
   let ctx = [];
 
   for (let i = 0; i < 3; i++) {
     canvas[i] = document.createElement(`canvas`);
+    canvas[i].width = axisWidth[i][0] * sizeCoefficient;
+    canvas[i].height = axisWidth[i][1] * sizeCoefficient;
     ctx[i] = canvas[i].getContext('2d');
     ctx[i].imageSmoothingEnabled = false;
-
     texture[i] = new THREE.Texture(canvas[i]);
 
     createCanvasBcg(
       canvas[i],
       ctx[i],
-      'red',
+      color,
       axisQuantity[i][0],
       axisQuantity[i][1],
     );
+    let maxWidth = canvas[i].width - 10;
+
+    if (maxWidth >= 350 && i === 1) {
+      ctx[i].font = '20px Arial';
+      ctx[i].fillStyle = '#fff';
+      wrapText(ctx[i], text, 10, 30, maxWidth, 22);
+    }
     texture[i].needsUpdate = true;
   }
 
@@ -92,53 +135,156 @@ export const createBox = (scene, { size, complex, position }) => {
 
   cube.position.set(
     convertCoordinatesCube(posX, xw),
-    convertCoordinatesCube(posY, yw),
     convertCoordinatesCube(posZ, zw),
+    convertCoordinatesCube(posY, yw),
   );
 };
 
-export const createCylinder = (scene) => {
+export const createCylinder = (
+  scene,
+  { height, radius, complex, location, uid },
+) => {
+  const color = getRandomColor();
+
+  const convertData = {
+    convertHeight: height / sizeCoefficient,
+    convertRadius: radius / sizeCoefficient,
+    quantity: complex.z,
+    posX: location.x / sizeCoefficient,
+    posY: location.y / sizeCoefficient,
+    posZ: location.z / sizeCoefficient,
+    text: uid,
+  };
+
+  const {
+    convertHeight,
+    convertRadius,
+    quantity,
+    posX,
+    posY,
+    posZ,
+    text,
+  } = convertData;
+
   const createCanvasBcg = (canvas, ctx, color = '#0088ff', amount) => {
     for (let i = 0; i <= amount; i++) {
       ctx.fillStyle = color;
       ctx.fillRect(
         0,
         (i * canvas.height) / amount,
-        361,
+        canvas.width,
         canvas.height / amount,
       );
     }
     for (let i = 0; i <= canvas.height; i += canvas.height / amount) {
-      console.log((i * canvas.height) / amount);
       ctx.moveTo(0, i);
       ctx.lineTo(canvas.width, i);
+      ctx.lineWidth = 2;
       ctx.strokeStyle = '#fff';
       ctx.stroke();
     }
   };
 
   const canvas = document.createElement(`canvas`);
+
+  canvas.width = Math.PI * convertRadius * 2 * sizeCoefficient;
+  canvas.height = convertHeight * sizeCoefficient;
   const ctx = canvas.getContext('2d');
   const texture = new THREE.Texture(canvas);
   texture.needsUpdate = true;
   ctx.imageSmoothingEnabled = false;
 
-  createCanvasBcg(canvas, ctx, '#0088ff', 10);
+  createCanvasBcg(canvas, ctx, color, quantity);
 
+  let maxWidth = canvas.width - 10;
+  ctx.font = '5px Arial';
+  ctx.fillStyle = '#fff';
+  wrapText(ctx, text, 5, 8, maxWidth, 32);
   let cylinderMaterials = [
     new THREE.MeshBasicMaterial({ map: texture }),
-    new THREE.MeshBasicMaterial({ color: 0x0088ff }),
-    new THREE.MeshBasicMaterial({ color: 0x0088ff }),
+    new THREE.MeshBasicMaterial({ color: color }),
+    new THREE.MeshBasicMaterial({ color: color }),
   ];
-  let cylinderGeometry = new THREE.CylinderGeometry(5, 5, 10, 361);
+
+  let cylinderGeometry = new THREE.CylinderGeometry(
+    convertRadius,
+    convertRadius,
+    convertHeight,
+    20,
+  );
 
   let cylinder = new THREE.Mesh(cylinderGeometry, cylinderMaterials);
 
-  cylinder.position.set(
-    convertCoordinatesCube(0, 5),
-    convertCoordinatesCube(0, 10),
-    convertCoordinatesCube(0, 5),
-  );
+  // cylinder.position.set(
+  //   convertCoordinatesCube(posX, convertRadius),
+  //   convertCoordinatesCube(posZ, convertRadius),
+  //   convertCoordinatesCube(posY, convertHeight),
+  // );
+  cylinder.position.set(posX, posZ + convertHeight / 2, posY);
 
   scene.add(cylinder);
 };
+
+export function showResult(scene, containerObject) {
+  let palletSize = {
+    x: containerObject.size.x / containerObject.complex.x,
+    y: containerObject.size.y / containerObject.complex.y,
+    z: containerObject.size.z / containerObject.complex.z,
+  };
+
+  let pallet = containerObject;
+  // if (pallet.type == 'pallet') {
+  //   let data = {
+  //     size: containerObject.size,
+  //     complex: containerObject.complex,
+  //     location: containerObject.location,
+  //     uid: containerObject.uid,
+  //   };
+  //   createBox(scene, data);
+  // }
+
+  for (let x = 0; x < pallet.complex.x; ++x) {
+    for (let y = 0; y < pallet.complex.y; ++y) {
+      for (let z = 0; z < pallet.complex.z; ++z) {
+        for (let j in pallet.children) {
+          let obj = pallet.children[j];
+
+          let boxLocation = {
+            x: obj.location.x + x * palletSize.x,
+            y: obj.location.y + y * palletSize.y,
+            z: obj.location.z + z * palletSize.z,
+          };
+          let rollLocation = {
+            x: obj.location.x + x * palletSize.x,
+            y: obj.location.y + y * palletSize.y,
+            z: obj.location.z + z * palletSize.z,
+          };
+
+          switch (obj.form) {
+            case 'box':
+              console.log(obj.type);
+              let boxData = {
+                size: obj.size,
+                complex: obj.complex,
+                location: boxLocation,
+                uid: obj.uid,
+              };
+              createBox(scene, boxData);
+              break;
+
+            case 'roll':
+              let rollData = {
+                height: obj.height,
+                radius: obj.radius,
+                complex: obj.complex,
+                location: rollLocation,
+                uid: obj.uid,
+              };
+              createCylinder(scene, rollData);
+              break;
+          }
+        }
+      }
+    }
+  }
+}
